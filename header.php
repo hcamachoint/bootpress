dc46400625a2c910f4ee0b2961e075163a7892bf
dunicode <dunicode@TUX.localdomain>
2020-05-16 19:59:32 -0400

    asd

--- header.php
+++ header.php
@@ -4,26 +4,14 @@
     <meta charset="utf-8">
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="MM Construction LLC was founded by Rafael A. Morros, III with over 10 years, post serving his country, managing complex residential and commercial real estate redevelopment projects. With hight experience in Residential & Commercial Pool Construction, Custom Home Design & Construction, Kitchens Remodeling.">
     <meta name="robots" content="index,follow">
-    <meta name="keywords" content="construction,constractor,builder,building,home,home builders,pool,pool builders,pool builders near me,pool construction,pool construction near me,building a pool deck">
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-152013000-1"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'UA-152013000-1');
-    </script>
     <title><?php wp_title('•', true, 'right'); bloginfo('name'); ?></title>
     <?php wp_head();?>
   </head>
   <body <?php body_class(); ?>>
     <header>
-
      <nav class="navbar navbar-expand-sm navbar-dark bg-nav shadow">
-       <h2 style="color:#000"><a href="/"><img src="<?php echo( get_header_image() ); ?>" alt="<?php echo( get_bloginfo( 'title' ) ); ?>" height="60px" style="margin-bottom:-10px;margin-top:0px;margin-right:10px"/></a></h2>
+       <h2 style="color:#000"><a href="/"><img src="<?php echo( get_header_image() ); ?>" alt="<?php echo( get_bloginfo( 'title' ) ); ?>" height="60px" style="margin-bottom:-10px;margin-top:0px;margin-right:20px"/></a></h2>
        <?php
          wp_nav_menu([
            'menu'            => 'top',
